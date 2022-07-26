bca181db329a3b26907728b2795f152472358768
Eriton Xavier <ertao1@hotmail.com>
2022-07-26 08:53:10 -0300

    Projeto Controle Fluxo com Exceção da DIO

---
+++ src/contador/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package contador;
+
+public class ParametrosInvalidosException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+		public ParametrosInvalidosException(String msg) {
+			super(msg);
+		}
+}
